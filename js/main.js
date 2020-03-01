327a8e16b1a2d01fadc6530ed45ea110763f529f
deleterPlanet <58425059+deleterPlanet@users.noreply.github.com>
2020-03-01 14:39:19 +0300

    Added opening menu items on click

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 articles = [["Git", "Git Команды", "Команды Git необходимые при командной разработки большого проекта."],
 			["Nodejs", "Команды Node.js для работы в консоли", "Полезные команды для работы с Node.js в консоли."]];
-var content = document.getElementById('content');
-var contentDiv,
+var content = document.getElementById('content'),
+	menu = document.getElementById('menu'),
+	lastOpen = null,
+	contentDiv,
 	img,
 	contentText,
 	h3,
@@ -34,4 +36,40 @@ for (var i = 0; i < articles.length; i++){
 	contentDiv.appendChild(contentText);
 
 	content.appendChild(contentDiv);
+}
+
+//установка функции onclick для эллементов меню
+for (var i = 0; i < menu.children.length; i++) {
+	for (var j = 0; j < menu.children[i].children[1].children.length; j++){
+		menu.children[i].children[1].children[j].children[0].style.display = 'none';
+	};
+    menu.children[i].onclick = function(){
+    	if ((lastOpen == null) || (lastOpen == this)){
+    		openClose(this);
+    	}else{
+    		openClose(lastOpen);
+    		openClose(this);
+    	}
+    };
+};
+
+//открыть или закрыть пункт меню
+async function openClose(e){
+	if (e.children[1].style.height == ""){
+		lastOpen = e;
+		e.children[1].style.height = 40*e.children[1].children.length + "px";
+		setDisplay(e.children[1]);
+	}else{
+		lastOpen = null;
+		e.children[1].style.height = "";
+		setTimeout(setDisplay, 500*e.children[1].children.length, e.children[1]);
+	}
+	return false;
+}
+
+//установить display на none если элемент виден или на block если элемент скрыт
+async function setDisplay(o){
+	for (var j = 0; j < o.children.length; j++){
+		o.children[j].children[0].style.display = (o.children[j].children[0].style.display == 'none')? 'block': 'none';
+	}
 }
